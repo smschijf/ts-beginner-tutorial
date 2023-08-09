783ca254227828fbee74900e9ceb175fdc609b68
smschijf <210578@student.glu.nl>
2023-08-09 15:05:02 +0200

    updated counter with CounterProps

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import Heading from "./components/Heading";
 import { Section } from "./components/Section";
 import Counter from "./components/Counter";
 
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState<number>(1);
+
   return (
     <>
       <Heading title={"Hello World!"} />
       <Section title={"Different Title"}>This is my section</Section>
-      <Counter />
+      <Counter setCount={setCount}>Count is {count}</Counter>
     </>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { ReactNode } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState<number>(1);
+type CounterProps = {
+    setCount: React.Dispatch<React.SetStateAction<number>>,
+    children: ReactNode
+}
+
+const Counter = ({ setCount, children }: CounterProps) => {
 
   return (
     <>
-      <h1>Count is {count}</h1>
+      <h1>{children}</h1>
       <button onClick={() => setCount(prev => prev + 1)}>+</button>
       <button onClick={() => setCount(prev => prev - 1)}>-</button>
     </>
