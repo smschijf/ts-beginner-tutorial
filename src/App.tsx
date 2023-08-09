e1d667deb862949ab3eb2d94023633ec1866c96d
smschijf <210578@student.glu.nl>
2023-08-09 14:43:40 +0200

    added Section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Heading from "./components/Heading";
+import { Section } from "./components/Section";
 
 function App() {
   return (
-    <Heading title={"Hello World!"}/>
-  )
+    <>
+      <Heading title={"Hello World!"} />
+      <Section title={"Different Title"}>This is my section</Section>
+    </>
+  );
 }
 
 export default App;
