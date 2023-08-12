6b8123cdfa399f828bdea00a519c4e7f4a53ca19
smschijf <210578@student.glu.nl>
2023-08-12 13:05:27 +0200

    added List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Heading from "./components/Heading";
 import { Section } from "./components/Section";
 import Counter from "./components/Counter";
+import List from "./components/List";
 
 import { useState } from "react";
 
@@ -12,6 +13,10 @@ function App() {
       <Heading title={"Hello World!"} />
       <Section title={"Different Title"}>This is my section</Section>
       <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["Cappuccino", "Anime", "Code"]}
+        render={(item: string) => <span className="gold">{item}</span>}
+      />
     </>
   );
 }
