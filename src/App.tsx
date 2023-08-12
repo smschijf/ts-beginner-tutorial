15a51f88d528b83e17ef0a95a9b5fa0bffd3df1b
smschijf <210578@student.glu.nl>
2023-08-12 13:34:28 +0200

    added useeffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+interface User {
+  id: number,
+  username: string
+}
 
 function App() {
   const [count, setCount] = useState<number>(1);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    console.log("mounting");
+    console.log("Users: ", users)
+
+    return () => console.log("unmounting")
+  }, [users])
 
   return (
     <>
